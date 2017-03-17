d867974606d9cb144624e26342919dd505d95e72
Bartłomiej Semańczyk <bartekss2@icloud.com>
2017-03-17 07:55:44 +0100

    Update example for foreground notifications.

---
+++ Example/Pods/Target Support Files/BSForegroundNotification/BSForegroundNotification-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_BSForegroundNotification : NSObject
+@end
+@implementation PodsDummy_BSForegroundNotification
+@end
---
+++ Example/Pods/Target Support Files/BSForegroundNotification/BSForegroundNotification-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double BSForegroundNotificationVersionNumber;
+FOUNDATION_EXPORT const unsigned char BSForegroundNotificationVersionString[];
+
